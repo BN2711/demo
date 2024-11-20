3d0d2e018ddfb55954552687ae9fdba53f520cd0
BN2711 <davethanhdai@gmail.com>
2024-11-20 21:12:11 +0700

    Update logout.php

--- manager_users/modules/auth/logout.php
+++ manager_users/modules/auth/logout.php
@@ -2,3 +2,15 @@
 if(!defined('_CODE')){
     die('Access denied...');
 }
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Tiến hành xử lý đăng xuất ở đây
+session_unset(); // Hủy các biến session
+session_destroy(); // Hủy session
+
+// Chuyển hướng về trang đăng nhập hoặc trang khác
+redirect('?module=auth&&action=login');
+?>
